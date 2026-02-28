38e619b1bef005f0a0924f913d8c2dd1ce4c240e
Zachary Baur <zachary.baur@gmail.com>
2026-02-28 18:13:11 +0000

    working on getting browser router working so the site has multi-page functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
+import HowToPlay from './How-To-Play';
 
 
 
 function App() {
 
   return (
-    <>
-      <nav class="top-nav">
-        <ul class="logo-button">
+    <BrowserRouter>
+      <nav className="top-nav">
+        <ul className="logo-button">
             <img src="" alt="" title=""/>
             <p>MSU Media & Info Slot Machine</p>
         </ul>
-        <ul class="nav-button">
-            <li><a href="/how-to-play.html" title="how-to-play">How to Play</a></li>
-            <li><a href="/professors.html" title="Professors">Professors</a></li>
+        <ul className="nav-button">
+            <li><Link to="/How-To-Play" title="How To Play">How To Play</Link></li>
+            <li><Link to="/Professors" title="Professors">Professors</Link></li>
         </ul>
     </nav>
-    <div class="slot-machine">
-        <h1>Casino</h1>
-        <ul class="slots">
-            <li><img/></li>
-            <li><img/></li>
-            <li><img/></li>
-        </ul>
-        <button class="spinbutton">SPIN</button>
+    <div className="slot-machine">
+        <Routes>
+            <Route path="/" element={<h1>Casino</h1>} />
+            <Route path="/How-To-Play" element={HowToPlay()} />
+            <Route path="/Professors" element={Professors()} />
+        </Routes>
     </div>
-    <div class="pastspins">
-        <h2>Past 5 Spins</h2>
-        <section class="spingrid">
-            <ul class="spinrow">
-                <li><img/></li>
-                <li><img/></li>
-                <li><img/></li>
-            </ul>
-            <ul class="spinrow">
-                <li><img/></li>
-                <li><img/></li>
-                <li><img/></li>
-            </ul>
-            <ul class="spinrow">
-                <li><img/></li>
-                <li><img/></li>
-                <li><img/></li>
-            </ul>
-            <ul class="spinrow">
-                <li><img/></li>
-                <li><img/></li>
-                <li><img/></li>
-            </ul>
-            <ul class="spinrow">
-                <li><img/></li>
-                <li><img/></li>
-                <li><img/></li>
-            </ul>
-        </section>
-    </div>
-    <buttone class="backtotopbutton">^</buttone>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
 export default App
