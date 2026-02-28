d9c92d921c59185c145fe524629edf544f14aec2
Zachary Baur <zachary.baur@gmail.com>
2026-02-28 17:50:57 +0000

    populated file with code copied from MI-Slot-Machine

---
+++ src/Professors.jsx
@@ -0,0 +1,138 @@
+import React from 'react';
+
+function Professors() {
+
+    return(
+        <div>
+            <div class="professorlist">
+        <div class="popupnav">
+            <h1>List of Professors</h1>
+            <button><a href="/index.html" title="home">X</a></button>
+        </div>
+        <div class="professorgrid">
+            <div class="professor">
+                <img src="/images/Jeff-Siarto.jpg" alt="Jeff Siarto" title="Jeff Siarto"/>
+                <figcaption>Jeff Siarto</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Young-Arygris.jpg" alt="Young Arygris" title="Young Arygris"/>
+                <figcaption>Young Arygris</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Susan-Bonner.jpg" alt="Susan Bonner" title="Susan Bonner"/>
+                <figcaption>Susan Bonner</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Sandra-Braman.jpg" alt="Sandra Braman" title="Sandra Braman"/>
+                <figcaption>Sandra Braman</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Celeste-Campos-Castillo.jpg" alt="Celeste Campos-Castillo" title="Celeste Campos-Castillo"/>
+                <figcaption>Celeste Campos-Castillo</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Amanda-Cote.jpg" alt="Amanda Cote" title="Amanda Cote"/>
+                <figcaption>Amanda Cote</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Andrew-Dennis.jpg" alt="Andrew Dennis" title="Andrew Dennis"/>
+                <figcaption>Andrew Dennis</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/David-Ewoldsen.jpg" alt="David Ewoldsen" title="David Ewoldsen"/>
+                <figcaption>David Ewoldsen</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Ricardo-Guimaraes.jpg" alt="Ricardo Guimaraes" title="Ricardo Guimaraes"/>
+                <figcaption>Ricardo Guimaraes</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Keith-Hampton.jpg" alt="Keith Hampton" title="Keith Hampton"/>
+                <figcaption>Keith Hampton</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Jean-Hardy.jpg" alt="Jean Hardy" title="Jean Hardy"/>
+                <figcaption>Jean Hardy</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Cory-Heald.jpg" alt="Cory Heald" title="Cory Heald"/>
+                <figcaption>Cory Heald</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Carrie-Heeter.jpg" alt="Carrie Heeter" title="Carrie Heeter"/>
+                <figcaption>Carrie Heeter</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Megan-Knittel.jpg" alt="Megan Knittel" title="Megan Knittel"/>
+                <figcaption>Megan Knittel</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Nick-Kwiatowski.jpg" alt="Nick Kwiatowski" title="Nick Kwiatowski"/>
+                <figcaption>Nick Kwiatowski</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Hee-Rin-Lee.jpg" alt="Hee Rin Lee" title="Hee Rin Lee"/>
+                <figcaption>Hee Rin Lee</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Megh-Marathe.jpg" alt="Megh Marathe" title="Megh Marathe"/>
+                <figcaption>Megh Marathe</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Casey-ODonnell.jpg" alt="Casey O'Donnell" title="Casey O'Donnell"/>
+                <figcaption>Casey O'Donnell</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Jennifer-Olson.jpg" alt="Jennifer Olson" title="Jennifer Olson"/>
+                <figcaption>Jennifer Olson</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Wei-Peng.jpg" alt="Wei Peng" title="Wei Peng"/>
+                <figcaption>Wei Peng</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Daniel-Putman.jpg" alt="Daniel Putman" title="Daniel Putman"/>
+                <figcaption>Daniel Putman</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Rabindra-Ratan.jpg" alt="Rabindra Ratan" title="Rabindra Ratan"/>
+                <figcaption>Rabindra Ratan</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Ruth-Shillair.jpg" alt="Ruth Shillair" title="Ruth Shillair"/>
+                <figcaption>Ruth Shillair</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Sarah-Swierenga.jpg" alt="Sarah Swierenga" title="Sarah Swierenga"/>
+                <figcaption>Sarah Swierenga</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Ryan-Thompson.jpg" alt="Ryan Thompson" title="Ryan Thompson"/>
+                <figcaption>Ryan Thompson</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Morgan-Vigil-Hayes.jpg" alt="Morgan Vigil-Hayes" title="Morgan Vigil-Hayes"/>
+                <figcaption>Morgan Vigil-Hayes</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Valeta-Wensloff.jpg" alt="Valeta Wensloff" title="Valeta Wensloff"/>
+                <figcaption>Valeta Wensloff</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Brian-Winn.jpg" alt="Brian Winn" title="Brian Winn"/>
+                <figcaption>Brian Winn</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/Susan-Wyche.jpg" alt="Susan Wyche" title="Susan Wyche"/>
+                <figcaption>Susan Wyche</figcaption>
+            </div>
+            <div class="professor">
+                <img src="/images/.jpg" alt="Jeff Siarto" title="Jeff Siarto"/>
+                <figcaption>Golden Jeff</figcaption>
+            </div>
+        </div>
+    </div>
+
+        </div>
+    )
+}
