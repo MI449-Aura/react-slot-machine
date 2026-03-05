8b0210a99c36d840e631126cefc69701fbc34cba
Zachary Baur <zachary.baur@gmail.com>
2026-03-05 01:32:17 +0000

    removed app functiona nd router functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react'
 import './App.css'
-import HowToPlay from './How-To-Play';
-import Professors from './Professors';
 
 function Home() {
     return(
@@ -51,31 +46,6 @@ function Home() {
             <button class="backtotopbutton">^</button>
         </div>
     )
-}
-
-function App() {
-
-  return (
-    <BrowserRouter>
-      <nav className="top-nav">
-        <ul className="logo-button">
-            <img src="" alt="" title=""/>
-            <p>MSU Media & Info Slot Machine</p>
-        </ul>
-        <ul className="nav-button">
-            <li><Link to="/How-To-Play" title="How To Play">How To Play</Link></li>
-            <li><Link to="/Professors" title="Professors">Professors</Link></li>
-        </ul>
-    </nav>
-    <div className="slot-machine">
-        <Routes>
-            <Route path="/" element={Home()} />
-            <Route path="/How-To-Play" element={HowToPlay()} />
-            <Route path="/Professors" element={Professors()} />
-        </Routes>
-    </div>
-    </BrowserRouter>
-  );
 }
 
 export default App
