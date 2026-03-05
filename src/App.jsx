68d86c74bc8d330b2fea39746ce34a1bd308b6b8
Zachary Baur <zachary.baur@gmail.com>
2026-03-05 01:41:19 +0000

    populated How To Play popup with content

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function Home() {
                     </li>
                 </ul>
             </nav>
-            
+
             <div class="slot-machine">
                 <h1>Casino</h1>
                 <ul class="slots">
@@ -61,6 +61,22 @@ function Home() {
                     </ul>
                 </section>
             </div>
+
+            {showHowToPlay && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <button className="close-button" onClick={() => setShowHowToPlay(false)}>X</button>
+                        <h1>How To Play</h1>
+                        <p>Click on the <strong>SPIN</strong> button to start the slot machine. It will give three random picutres of professors. If all three manage to be identical, you win the suprising reward... of visiting their offical MSU profile!</p>
+                        <ul class="spinrow">
+                            <li><img src="/images/Jeff-Siarto.jpg" alt="Jeff Siarto" title="Jeff Siarto"/></li>
+                            <li><img src="/images/Jeff-Siarto.jpg" alt="Jeff Siarto" title="Jeff Siarto"/></li>
+                            <li><img src="/images/Jeff-Siarto.jpg" alt="Jeff Siarto" title="Jeff Siarto"/></li>
+                        </ul>
+                        <p>If all three images are the world-renowned designer, author, and software developer Professor Jeff Siarto, you have hit the <strong>JACKPOT!!!</strong></p>
+                    </div>
+                </div>
+            )}
             <button class="backtotopbutton">^</button>
         </div>
     )
