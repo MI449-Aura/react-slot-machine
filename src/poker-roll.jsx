492e07b1abd70cef864c2563ee5358b990be4658
Angie <popeian1@msu.edu>
2026-03-14 20:40:47 +0000

    poker hand generator is now working

--- src/poker-roll.jsx
+++ src/poker-roll.jsx
@@ -6,53 +6,36 @@ function PokerRoll({Trigger}){
     let cardList = [];
     
     useEffect(() => {
-        fetch('https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/deck.json')
+        fetch('https://raw.githubusercontent.com/MI449-Aura/card-deck-api/main/deck.json')
             .then(res => res.json())
             .then(data => setDeck(data))
             .catch(err => console.error('Failed to load deck:', err));
     }, [Trigger]);
 
-    function drawFive(){
-        for(let i = 0; i < 5; i++){
-            const currentCard = showDeck[Math.floor(Math.random() * (showDeck.length - 1))];
-            if(cardList.includes(currentCard)){
-                i--;
-            }else{
-                cardList.push(currentCard);
-            }
-        }
-        return(cardList);
+    
+    for(let i = 0; i < 5; i++){
+        if (showDeck.length === 0) return null;
+        const currentCard = showDeck[Math.floor(Math.random() * showDeck.length)];
+        cardList.push(currentCard);
     }
 
-    const cards = drawFive();
-
-    var card_one = document.getElementsByClassName("card-1");
-    var card_two = document.getElementsByClassName("card-2");
-    var card_three = document.getElementsByClassName("card-3");
-    var card_four = document.getElementsByClassName("card-4");
-    var card_five = document.getElementsByClassName("card-5");
-
-    card_one.src = cardList[0].image;
-    card_two.src = cardList[1].image;
-    card_three.src = cardList[2].image;
-    card_four.src = cardList[3].image;
-    card_five.src = cardList[4].image;
+    const img_prefix_link = "https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/";
 
     return(
         <div id="cards">
             <div id="cards-sect-1">
-                <img className="card-1" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/red_joker.svg"></img>
-                <img className="card-2" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/black_joker.svg"></img>
-                <img className="card-3" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/red_joker.svg"></img>
-                <img className="card-4" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/black_joker.svg"></img>
-                <img className="card-5" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/red_joker.svg"></img>
+                <img src={img_prefix_link+cardList[0].image}></img>
+                <img src={img_prefix_link+cardList[1].image}></img>
+                <img src={img_prefix_link+cardList[2].image}></img>
+                <img src={img_prefix_link+cardList[3].image}></img>
+                <img src={img_prefix_link+cardList[4].image}></img>
             </div>
             <div id="cards-sect-2">
-                <img className="card-1" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/red_joker.svg"></img>
-                <img className="card-2" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/black_joker.svg"></img>
-                <img className="card-3" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/red_joker.svg"></img>
-                <img className="card-4" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/black_joker.svg"></img>
-                <img className="card-5" src="https://raw.githubusercontent.com/MI449-Aura/card-deck-api/refs/heads/main/images/red_joker.svg"></img>
+                <img src={img_prefix_link+cardList[0].image}></img>
+                <img src={img_prefix_link+cardList[1].image}></img>
+                <img src={img_prefix_link+cardList[2].image}></img>
+                <img src={img_prefix_link+cardList[3].image}></img>
+                <img src={img_prefix_link+cardList[4].image}></img>
             </div>
         </div>
     )
